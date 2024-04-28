75c0dbc51d27631cf0c18fc64bbfa3dd556ae8e9
akkumaranand <anand.keshri123.ak@gmail.com>
2024-04-29 04:56:40 +0530

    adharcard

--- src/pages/seller.tsx
+++ src/pages/seller.tsx
@@ -90,8 +90,9 @@ const Seller = () => {
               <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} placeholder="Enter your email address"
                 className="w-full border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-400" />
             </div>
+
             <div className="mb-4">
-              <label htmlFor="aadhar" className="block mb-1 font-bold">Enter Aadhar Card NO: :</label>
+              <label htmlFor="aadhar"  className="block mb-1 font-bold">Enter Aadhar Card NO: :</label>
               <input
                 type="number"
                 id="aadhar"
